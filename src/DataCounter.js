2b035772bae6a61ceb83be05f3fa63dd67eaa825
Ryan Carville <ryancarville@gmail.com>
2019-03-21 15:47:11 +0100

    DataCounter udpate

--- src/DataCounter.js
+++ src/DataCounter.js
@@ -1,49 +1,57 @@
 import React from 'react';
 import './DataCounter.css';
 import $ from 'jquery';
+import { clearImmediate } from 'timers';
 
 export default class DataCounter extends React.Component {
 	constructor(props) {
 		super(props);
+		this.rollData = this.rollData.bind(this);
+	}
+	componentDidMount() {
+		this.rollData = this.rollData;
+	}
+	componentWillUnmount() {
+		clearImmediate(this.rollData);
 	}
 
+	rollData() {
+		$(window).scroll(function() {
+			var hT = $('.dataCounter').offset().top,
+				hH = $('.dataCounter').outerHeight(),
+				wH = $(window).height(),
+				wS = $(this).scrollTop();
+			if (wS > hT + hH - wH) {
+				$('.counting').each(function() {
+					var $this = $(this),
+						countTo = $this.attr('data-count');
+					//text or image to be animated
+					$({ countNum: $this.text() }).animate(
+						{
+							countNum: countTo
+						},
+
+						{
+							duration: 1000,
+							easing: 'linear',
+							step: function() {
+								$this.text(Math.floor(this.countNum));
+							},
+							complete: function() {
+								$this.text(this.countNum);
+								//alert('finished');
+							}
+						}
+					);
+				});
+			}
+		});
+	}
 	render() {
 		// number count for stats, using jQuery animate
 
-		const rollData = () => {
-			$(window).scroll(function() {
-				var hT = $('#scroll-to').offset().top,
-					hH = $('#scroll-to').outerHeight(),
-					wH = $(window).height(),
-					wS = $(this).scrollTop();
-				if (wS > hT + hH - wH) {
-					$('.counting').each(function() {
-						var $this = $(this),
-							countTo = $this.attr('data-count');
-						//text or image to be animated
-						$({ countNum: $this.text() }).animate(
-							{
-								countNum: countTo
-							},
-
-							{
-								duration: 1000,
-								easing: 'linear',
-								step: function() {
-									$this.text(Math.floor(this.countNum));
-								},
-								complete: function() {
-									$this.text(this.countNum);
-									//alert('finished');
-								}
-							}
-						);
-					});
-				}
-			});
-		};
 		return (
-			<div className='dataCounter' id='scroll-to' onLoad={rollData}>
+			<div className='dataCounter' id='scroll-to' onLoad={this.rollData}>
 				<section
 					id='counter-stats'
 					class='wow fadeInRight'
